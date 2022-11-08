c7d642f8eb916a03fd6f8aa53482b25b0c0149c9
espiralverde <martinsosa@outlook.com>
2022-11-08 15:46:41 -0300

    Correcciones_Final

--- Desafio_3/contenedor.js
+++ Desafio_3/contenedor.js
@@ -1,36 +1,36 @@
 const fs = require('fs')
 
 class Contenedor {
-constructor(fileName) {
-    this.fileName = fileName
+constructor(archivo) {
+    this.archivo = archivo
 }
-async save(obj) {
+async save(object) {
     try {
-    const data = await fs.promises.readFile(`./${this.fileName}`, 'utf-8'),
+    const data = await fs.promises.readFile(`./${this.archivo}`, 'utf-8'),
         jsonData = JSON.parse(data)
 
     if (jsonData.length > 0) {
         const lastId = jsonData[jsonData.length - 1].id
-        obj.id = lastId + 1
+        object.id = lastId + 1
     } else {
-        obj.id = 1
+        object.id = 1
     }
 
-    jsonData.push(obj)
-    fs.writeFileSync(`./${this.fileName}`, JSON.stringify(jsonData, null, 2))
+    jsonData.push(object)
+    fs.writeFileSync(`./${this.archivo}`, JSON.stringify(jsonData, null, 2))
     } catch (err) {
     throw new Error(err)
     }
 }
 async getById(num) {
     try {
-    const data = await fs.promises.readFile(`./${this.fileName}`, 'utf-8'),
+    const data = await fs.promises.readFile(`./${this.archivo}`, 'utf-8'),
         jsonData = JSON.parse(data),
         found = jsonData.find(element => element.id === num)
     if (found) {
         return found
     } else {
-        console.log(`ID "${num}" not found`)
+        console.log(`ID "${num}" no existe`)
     }
     } catch (err) {
     throw new Error(err)
@@ -38,10 +38,9 @@ async getById(num) {
 }
 async getAll() {
     try {
-    const data = await fs.promises.readFile(`./${this.fileName}`, 'utf-8'),
+    const data = await fs.promises.readFile(`./${this.archivo}`, 'utf-8'),
         jsonData = JSON.parse(data)
-    //console.log(`All the data: ${JSON.stringify(jsonData, null, 2)}`)
-    //const stringData = JSON.stringify(jsonData, null, 2)
+    /* console.log(`${JSON.stringify(jsonData, null, 2)}`) */
     return jsonData
     } catch (err) {
     throw new Error(err)
@@ -49,24 +48,24 @@ async getAll() {
 }
 async deleteById(num) {
     try {
-    const data = await fs.promises.readFile(`./${this.fileName}`, 'utf-8'),
+    const data = await fs.promises.readFile(`./${this.archivo}`, 'utf-8'),
         jsonData = JSON.parse(data),
         foundIndex = jsonData.findIndex(element => element.id === num)
     if (foundIndex !== -1) {
         jsonData.splice(foundIndex, 1)
         fs.writeFileSync(
-        `./${this.fileName}`,
+        `./${this.archivo}`,
         JSON.stringify(jsonData, null, 2)
         )
     } else {
-        console.log(`ID "${num}" not found`)
+        console.log(`ID "${num}" no existe`)
     }
     } catch (err) {
     throw new Error(err)
     }
 }
 deleteAll() {
-    fs.writeFileSync(`./${this.fileName}`, '[]')
+    fs.writeFileSync(`./${this.archivo}`, '[]')
 }
 }
 
--- Desafio_3/server.js
+++ Desafio_3/server.js
@@ -2,37 +2,31 @@ const express = require('express')
 require('dotenv').config()
 const Contenedor = require('./contenedor')
 
-const products = new Contenedor('products.txt')
+const products = new Contenedor('productos.txt')
 const app = express()
-let count = 0
+const PORT = process.env.PORT || 3000
+
 
 app.get('/', (_req, res) => {
-res.send({
-    health: 'Up',
-    success: true
-})
+    res.send ({
+        health: "Up",
+        success: true
+    })
 })
 
 app.get('/productos', async (_req, res) => {
-const productsAll = await products.getAll()
-
-res.send(`${JSON.stringify(productsAll, null, 2)}`)
+    const productosTodos = await products.getAll()
+    res.send(`${JSON.stringify(productosTodos, null, 2)}`)  
 })
 
 app.get('/productoRandom', async (_req, res) => {
-const productsAll = await products.getAll(),
-    prodsAmount = productsAll.length,
-    randomIndex = Math.floor(Math.random() * prodsAmount)
 
-res.send(`${JSON.stringify(productsAll[randomIndex], null, 2)}`)
+    const productosTodos = await products.getAll()
+    const randomIndex = Math.floor(Math.random() * productosTodos.length)
+    res.send(`${JSON.stringify(productosTodos[randomIndex], null, 2)}`)
+
 })
 
-app.get('/ping', (_req, res) => {
-res.send('<h1>Pong</h1>')
-})
-
-const PORT = process.env.PORT || 3000
-
-const service = app.listen(PORT, () => {
-console.info(`Server Up and running on port ${PORT}`)
+const server = app.listen(PORT, () => {
+    console.info(`Server up and running on Port ${PORT}`)
 })
